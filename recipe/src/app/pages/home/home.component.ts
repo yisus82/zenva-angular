89b4d16f8c6ce0a934e45b89e884cde600fd630a
Jesús Ángel Pérez-Roca Fernández <yisus82@users.noreply.github.com>
2020-02-07 12:56:21 +0100

    Navbar + Home component

--- recipe/src/app/pages/home/home.component.ts
+++ recipe/src/app/pages/home/home.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(public recipeService: RecipeService) { }
 
 }
