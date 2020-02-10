a36264b5737951537cffa70e3cd705d469de5eaa
Jesús Ángel Pérez-Roca Fernández <yisus82@users.noreply.github.com>
2020-02-10 11:10:08 +0100

    Show recipe component

--- recipe/src/app/pages/show-recipe/show-recipe.component.ts
+++ recipe/src/app/pages/show-recipe/show-recipe.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { IRecipe } from '../../interfaces/recipe';
+import { RecipeService } from './../../services/recipe.service';
 
 @Component({
   selector: 'app-show-recipe',
   templateUrl: './show-recipe.component.html',
   styleUrls: ['./show-recipe.component.scss']
 })
-export class ShowRecipeComponent implements OnInit {
+export class ShowRecipeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscriptions: Subscription[] = [];
+  public recipe: IRecipe;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private recipeService: RecipeService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.subscriptions.push(
+      this.route.paramMap.subscribe(params => {
+        const recipeId = params.get('id');
+        this.recipe = this.recipeService.getRecipeById(parseInt(recipeId, 10));
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  back() {
+    this.location.back();
+  }
+
+  deleteRecipe(): void {
+    this.recipeService.deleteRecipe(this.recipe.id);
+    this.router.navigate(['']);
   }
 
 }
